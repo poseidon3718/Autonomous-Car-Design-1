741977958014587eb9307e34b89703138d8394f4
poseidon3718 <85330911+poseidon3718@users.noreply.github.com>
2024-03-11 09:43:28 +0900

    Add files via upload

---
+++ 20240311/Code1/Code1.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  // 설정 함수
+  Serial.begin(9600); // 통신 속도를 9600으로 변경
+  // Serial.println("test1");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // 반복 실행 함수
+  Serial.println("test1");
+}

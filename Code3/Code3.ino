ade8c6a529f65389d316bc0002cd75b7bcbaea8f
poseidon3718 <85330911+poseidon3718@users.noreply.github.com>
2024-03-11 16:39:44 +0900

    Add files via upload

---
+++ Code3/Code3.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello World!!");
+  Serial.println("Hello World!!");
+  Serial.println(77);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
